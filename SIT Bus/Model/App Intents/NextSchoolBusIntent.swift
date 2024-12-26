8c899b440c06f41ba14e0fdc791ce538288bdabb
Yuto Iwakami <11906960+yidev0@users.noreply.github.com>
2024-12-26 17:01:57 +0900

    Complete NextSchoolBusIntent

--- SIT Bus/Model/App Intents/NextSchoolBusIntent.swift
+++ SIT Bus/Model/App Intents/NextSchoolBusIntent.swift
@@ -9,23 +9,38 @@ import AppIntents
 
 struct NextSchoolBusIntent: AppIntent {
     
-    static var title: LocalizedStringResource = .init("GetNextBus.Title", table: "Shortcuts")
+    static var title: LocalizedStringResource = .init("GetNextBus.Title", table: "Intents")
+    static var description: IntentDescription? = .init(.init("GetNextBus.Description", table: "Intents"))
     
-    @Parameter(title: "")
+    @Parameter(title: "Label.BusType")
     var busType: BusLineType.SchoolBus
     
-    @Parameter(default: .now)
+    @Parameter(title: "Label.Date", default: .now)
     var date: Date
     
     static var parameterSummary: some ParameterSummary {
-        Summary("NextSchoolBus.Summary", table: "Shortcuts") {
-            \.$date
+        Summary("NextSchoolBusFrom\(\.$date).Summary", table: "Intents") {
             \.$busType
         }
     }
     
-    func perform() async throws -> some IntentResult {
-        .result()
+    func perform() async throws -> some IntentResult & ReturnsValue<String> {
+        let schoolBusData = await BusDataFetcher().fetchLocalData()
+        switch schoolBusData {
+        case .success(let success):
+            let timetable = success.makeTimetable(for: busType, date: date)
+            if let nextBus = timetable?.getNextBus(for: date) {
+                let dateFormatter = DateFormatter()
+                dateFormatter.timeZone = .autoupdatingCurrent
+                dateFormatter.timeStyle = .medium
+                dateFormatter.dateStyle = .medium
+                return .result(value: dateFormatter.string(from: nextBus))
+            } else {
+                return .result(value: "Label.BusServiceEnded".localize)
+            }
+        case .failure(let failure):
+            throw failure
+        }
     }
     
 }
